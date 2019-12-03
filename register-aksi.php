458825a635effd2089fd4555109611c040fc0d43
Shofiyah Mardhiyah <54921573+Shofi52@users.noreply.github.com>
2019-12-03 21:30:03 +0700

    Adding packages to register-aksi.php

--- register-aksi.php
+++ register-aksi.php
@@ -1,3 +1,4 @@
+
 <?php
 include 'connection.php';
 
@@ -27,3 +28,79 @@ $query = mysqli_query($host,"INSERT INTO booking_details(destination, pickup_tim
 
         }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bon Voyage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style type="text/css">
+  label + label {
+    margin-left: 50px;
+}
+</style>
+</head>
+<body> 
+<div class= "judul"> 
+ <h1>Packages</h1>
+ </div>
+ <br>
+<div class="container">
+<form name ="form1" method ="post" action ="book.php">
+  <div class="card-deck">
+   <td>
+   <div class="card" style="width:400px">
+   <label> 
+    <div class="card-body">
+      <h4 class="card-title">Package 1</h4>
+      <p class="card-text">Description</p>
+      <!-- <input type="radio" name="package" value="1" required> -->
+      <!-- <a href="book.php" class="btn btn-primary">See Profile</a> -->
+      <input type="radio" name="package" value = "1" required/>
+    </div>
+    </label>
+    </div>
+    <div class="card" style="width:400px">
+   <label> 
+    <div class="card-body">
+      <h4 class="card-title">Package 2</h4>
+      <p class="card-text">Description</p>
+      <!-- <input type="radio"  name="package" value="2" required> -->
+      <!-- <a href="book.php" class="btn btn-primary">See Profile</a> -->
+      <input type="radio" name="package" value = "2" required/>
+    </div>
+    </label>
+    </div>
+    
+    <div class="card" style="width:400px">
+   <label> 
+    <div class="card-body">
+      <h4 class="card-title">Package 3</h4>
+      <p class="card-text">Description</p>
+      <!-- <input type="radio" name="package" value="3" required> -->
+      <!-- <a href="" class="btn btn-primary">See Profile</a> -->
+      <input type="radio" name="package" value = "3" required/>
+    </div>
+    </label>
+    </div>
+    <br>
+
+    </td>
+    <!-- <img class="card-img-bottom" src="img_avatar6.png" alt="Card image" style="width:100%"> -->
+  </div>
+  
+    <br>
+ 
+  <Input type = "Submit" style="login100-form-btn" Name = "Submit1" VALUE = "Book Now">
+  </div>
+</div>
+</form>
+
+
+</body>
+</html>

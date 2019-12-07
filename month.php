5400813106a83a4f2f080798201e4fdc24fa007e
anggunw <anggunwahyuni51@gmail.com>
2019-12-07 12:23:34 +0700

    added month.php , nationality.php

---
+++ month.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Transaction per Month</title>
+	<style>
+		table, tr, th, td
+		{
+			border: 1px solid black;
+		}
+	</style>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>Month</th>
+				<th>Total Transaction</th>
+		</tr>
+		<?php
+			$conn = mysqli_connect("localhost", "root", "", "bonvoyage");
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+			$sql = "SELECT month(pickup_time), count(book_number) FROM booking_details group by month(pickup_time) order by count(book_number)";
+			$result = $conn->query($sql);
+			if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+				echo "<tr>
+						<td>". $row["month(pickup_time)"]. "</td>
+						<td>" . $row["count(book_number)"] . "</td>
+						</tr>";
+				}
+				echo "</table>";
+			} else { echo "0 results"; }
+			$conn->close();
+		?>
+	</table>
+</head>
+</body>
+</html>
---
+++ nationality.php
@@ -0,0 +1,56 @@
+<?php
+
+if(isset($_POST['search'])){
+	$valueToSearch = $_POST['valueToSearch'];
+	$query = "SELECT book_date, destination, package_id_fk FROM booking_details WHERE email_fk IN (SELECT email FROM personal_identity WHERE nationality = '$valueToSearch')";
+	$search_result = filterTable($query);
+
+} else{
+	$query = "SELECT book_date, destination, package_id_fk FROM booking_details WHERE email_fk = ''";
+	$search_result = filterTable($query);
+}
+function filterTable($query){
+	$connect = mysqli_connect("localhost", "root", "", "bonvoyage");
+
+	if(mysqli_connect_errno()){
+		die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+	}
+	$filter_Result = mysqli_query($connect, $query) or die(mysqli_error($connect));
+	return $filter_Result;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bon Voyage Booking Search</title>
+	<style>
+		table, tr, th, td
+		{
+			border: 1px solid black;
+		}
+	</style>
+</head>
+<body>
+	<form action="nationality.php" method="post">
+		<input type="radio" name="valueToSearch" value="i">Indonesian
+		<input type="radio" name="valueToSearch" value="a">Others<br><br>
+		<input type="submit" name="search" value="Search"><br><br>
+		<table>
+			<tr>
+				<th>book made</th>
+				<th>destination</th>
+				<th>package</th>
+			</tr>
+			<?php while($row = mysqli_fetch_array($search_result)):?>
+			<tr>
+				<td><?php echo $row['book_date'];?></td>
+				<td><?php echo $row['destination'];?></td>
+				<td><?php echo $row['package_id_fk'];?></td>
+ 			</tr>
+ 		<?php endwhile;?>
+		</table>
+	</form>
+
+</body>
+</html>

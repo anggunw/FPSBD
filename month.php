5cb544ede0ee9e166f6b880bdea1a545584553ce
anggunw <anggunwahyuni51@gmail.com>
2019-12-09 21:05:29 +0700

    edit month.php, show month name

--- month.php
+++ month.php
@@ -21,13 +21,50 @@
 			if ($conn->connect_error) {
 				die("Connection failed: " . $conn->connect_error);
 			}
-			$sql = "SELECT month(pickup_time), count(book_number) FROM booking_details group by month(pickup_time) order by count(book_number)";
+			$sql = "SELECT month(pickup_time), count(book_number) FROM booking_details GROUP BY month(pickup_time) ORDER BY count(book_number) DESC";
 			$result = $conn->query($sql);
 			if ($result->num_rows > 0) {
 				// output data of each row
 				while($row = $result->fetch_assoc()) {
+				$bulan = $row["month(pickup_time)"];
+				if($bulan == 1){
+					$bulan = 'January';
+				}
+				if($bulan == 2){
+					$bulan = 'February';
+				}
+				if($bulan == 3){
+					$bulan = 'March';
+				}
+				if($bulan == 4){
+					$bulan = 'April';
+				}
+				if($bulan == 5){
+					$bulan = 'May';
+				}
+				if($bulan == 6){
+					$bulan = 'June';
+				}
+				if($bulan == 7){
+					$bulan = 'July';
+				}
+				if($bulan == 8){
+					$bulan = 'August';
+				}
+				if($bulan == 9){
+					$bulan = 'September';
+				}
+				if($bulan == 10){
+					$bulan = 'October';
+				}
+				if($bulan == 11){
+					$bulan = 'November';
+				}
+				if($bulan == 12){
+					$bulan = 'December';
+				}
 				echo "<tr>
-						<td>". $row["month(pickup_time)"]. "</td>
+						<td>". $bulan . "</td>
 						<td>" . $row["count(book_number)"] . "</td>
 						</tr>";
 				}

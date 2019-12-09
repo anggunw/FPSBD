ee2cad9c895ebeb6151728da70fdff4e4367aa34
liizzasulistio <58472359+liizzasulistio@users.noreply.github.com>
2019-12-09 20:55:22 +0700

    UI belum

---
+++ bonvoyage-statistics.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bon Voyage Statistics</title>
+		<link rel="stylesheet" type="text/css" href="css/registration.css">
+   		<link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+     	<link rel="stylesheet" type="text/css" href="css/registration.css">
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+</head>
+<body>
+	<div class="title">
+		<h1>Bon Voyage Statistics</h1>
+	</div>
+	<form action="memorable-cust.php" method="post">
+		<div class="container-login100-form-btn">
+		<button class="login100-form-btn">
+				<a href="memorable-cust.php" class="login100-form-btn">Our Memorable Customers</a>
+		</button>
+		</div>
+
+		<div class="container-login100-form-btn">
+		<button class="login100-form-btn">
+				<a href="mc-payment.php" class="login100-form-btn">Most Chosen Payment Method</a>
+		</button>
+		</div>
+
+		<div class="container-login100-form-btn">
+		<button class="login100-form-btn">
+				<a href="mc-package.php" class="login100-form-btn">Most Chosen Package</a>
+		</button>
+		</div>
+	</form>
+</body>
+</html>
---
+++ mc-package.php
@@ -0,0 +1,44 @@
+<?php
+$query = "SELECT package_name, COUNT(package_name) AS total_package FROM package, booking_details WHERE booking_details.package_id_fk = package.package_id GROUP BY package_name";
+	$search_result = filterTable($query);
+function filterTable($query){
+	$connect = mysqli_connect("localhost", "root", "", "bonvoyage");
+	if(mysqli_connect_errno()){
+		die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+	}
+	$filter_Result = mysqli_query($connect, $query) or die(mysqli_error($connect));
+	return $filter_Result;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Most Chosen Package</title>
+		<link rel="stylesheet" type="text/css" href="css/registration.css">
+   		<link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+     	<link rel="stylesheet" type="text/css" href="css/registration.css">
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+</head>
+<body>
+	<div class="title">
+		<h1>Most Chosen Package</h1>
+	</div>
+	<form action="mc-package.php" method="post">
+	<table>
+			<tr>
+				<th>Package</th>
+				<th>Total Package</th>
+			</tr>
+			<?php while($row = mysqli_fetch_array($search_result)):?>
+			<tr>
+				<td><?php echo $row['package_name'];?></td>
+				<td><?php echo $row['total_package'];?></td>
+ 			</tr>
+ 		<?php endwhile;?>
+		</table>	
+	</form>
+</body>
+</html>

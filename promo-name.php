7f3fb9e033e6f6049b66cb283b8d5a854d1771e7
anggunw <anggunwahyuni51@gmail.com>
2019-12-09 19:09:34 +0700

    add promo-name.php

---
+++ promo-name.php
@@ -0,0 +1,50 @@
+<?php
+	$query = "SELECT book_number, name, destination, package_name, date(pickup_time) FROM booking_details, personal_identity, package WHERE booking_details.email_fk = personal_identity.email AND booking_details.package_id_fk = package.package_id AND LOWER(personal_identity.name) LIKE '%lisa%' OR LOWER(personal_identity.name) LIKE '%shofi%' OR LOWER(personal_identity.name) LIKE '%anggun%'";
+	$result = showRes($query);
+
+function showRes($query){
+	$connect = mysqli_connect("localhost", "root", "", "bonvoyage");
+
+	if(mysqli_connect_errno()){
+		die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+	}
+	$res = mysqli_query($connect, $query) or die(mysqli_error($connect));
+	return $res;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Opening Promo</title>
+	<style>
+		table, tr, th, td
+		{
+			border: 1px solid black;
+		}
+	</style>
+</head>
+<body>
+	Dibawah ini adalah list reservasi yang mendapatkan promo opening 10% cashback dengan nama pemesan "lisa", "shofi" atau "anggun"<br>
+	Silahkan tunjukkan halaman ini dan KTP saat hari perjalanan<br>
+	<table>
+		<tr>
+			<th>Book Number</th>
+			<th>Name</th>
+			<th>Destination</th>
+			<th>Package</th>
+			<th>Travel Date</th>
+		</tr>
+		<?php while($row = mysqli_fetch_array($result)):?>
+		<tr>
+			<td><?php echo $row['book_number'];?></td>
+			<td><?php echo $row['name'];?></td>
+			<td><?php echo $row['destination'] === 'S' ? 'Surabaya' : 'Malang';?></td>
+			<td><?php echo $row['package_name'];?></td>
+			<td><?php echo $row['date(pickup_time)'];?></td>
+		</tr>
+	<?php endwhile;?>
+	</table>
+</head>
+</body>
+</html>
